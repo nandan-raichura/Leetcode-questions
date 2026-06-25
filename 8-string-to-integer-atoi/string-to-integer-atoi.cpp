8f91e812af615ac410a77c3c0a119c37d87f4827
Nandan Raichura <nandanraichura4@gmail.com>
2026-06-25 23:36:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (55.35%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        if(s.length()==0){
+            return 0;
+        }
+
+        // ignoreing the white space
+
+        int i=0;
+        while(i<s.size() && s[i]==' '){
+            i++;
+        }
+
+        s=s.substr(i); // substr from i to n
+
+        int sign=+1;
+        long ans=0;
+
+        if(s[0]=='-')sign=-1;
+
+        i=(s[0]=='-' || s[0]=='+')?1:0;// checking postion to start the iterations
+
+        int MAX=INT_MAX; int MIN=INT_MIN;
+
+        while(i<s.length()){
+            if(s[0]==' ' || !isdigit(s[i])){
+                break;
+            }
+            ans=ans*10+(s[i]-'0');
+
+            if(sign ==-1 && -1*ans<MIN)return MIN;
+            if(sign==+1 && ans>MAX)return MAX;
+
+
+            i++;
+        }
+
+        return (int)(sign*ans);
+    }
+};
