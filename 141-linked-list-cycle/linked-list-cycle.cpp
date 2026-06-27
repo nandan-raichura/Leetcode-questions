c451e6cc8b82c711fb621f6c636fa16613f5d45d
Nandan Raichura <nandanraichura4@gmail.com>
2026-06-27 14:05:10 +0530

    Time: 12 ms (43.61%) | Memory: 11.8 MB (53.51%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:// tortoise and hare algo
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+
+            // if it any stabe both combines /colides then the cycle exist we are makein slow move by one and fast by two and then at evry iteration the distance between them get reduced by one, so they will definetly meet
+
+            if(slow==fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
