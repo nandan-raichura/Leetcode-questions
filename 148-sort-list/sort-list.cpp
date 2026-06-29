a8444e66f3215e7183e71194da5bb3a6a81cfd81
Nandan Raichura <nandanraichura4@gmail.com>
2026-06-29 14:29:20 +0530

    Time: 54 ms (18.17%) | Memory: 75.6 MB (41.80%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,27 +10,77 @@
  */
 class Solution {
 public:
+    
+    ListNode* findMiddle(ListNode* head){// tortosie and hare algo to find the middle
+        ListNode* slow=head;
+        ListNode* fast=head->next;// we make changes here for the fast becz if the size of the linked list is the  oeven , we want that middle 1 , so for that we have made this changes
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        return slow;// slow will stop at the middle of the ll
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+      ListNode* t1=list1;
+      ListNode* t2=list2;
+
+
+      ListNode* dummy = new ListNode(-1);
+
+      ListNode* temp=dummy;
+
+
+      while(t1!=NULL && t2!=NULL){
+        if(t1->val < t2->val){
+            temp->next=t1;
+            temp=t1;
+            t1=t1->next;
+        }
+        else{
+            temp->next=t2;
+            temp=t2;
+            t2=t2->next;
+
+        }
+      }  
+
+      if(t1){// it means  t1 is still remaining so we will attach the temp pointer to the t1 (imp edge case)
+            temp->next=t1;
+      }else{
+            temp->next=t2;
+      }
+
+      return dummy->next;//dummy node is the main elmenet who have helped to bulit the entire linked list
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int> nums;
+        
+        if(head==NULL || head->next==NULL){
+            return head;
+        }// base case, when linked will be divided unitl the non sperable part
 
-         ListNode* temp=head;
+        ListNode* middle=findMiddle(head);
 
-        while(temp!=NULL){
-            nums.push_back(temp->val);
+        // after finding the ,middle , we will divde the ll into the n/2 part
 
-            temp=temp->next;
-        }
+        ListNode* leftHead=head;
+        ListNode* rightHead=middle->next;// at this steps we are seprating the ll for the further division 
 
-        sort(nums.begin(),nums.end());
+        middle->next=NULL;// to seprate the left side ll with right side one
 
-        temp=head;
-        int i=0;
-        while(temp!=NULL){
-            temp->val=nums[i];
-            temp=temp->next;
-            i++;
-        }
+        // now calling tthe function itself to compelete the further steps
 
-        return head;
+        leftHead=sortList(leftHead);// this will return the sorted head of the left side
+        rightHead= sortList(rightHead);// vice versa
+
+        // those above two recursive step will divide the ll into the parts at each level of the recurssion
+
+       ListNode* newNode= mergeTwoLists(leftHead,rightHead);// will combine both ll into one sorted list
+
+        return newNode;
     }
 };
